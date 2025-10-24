2a62f0a200d86e0dd374029705d89aba6f7bee0c
Prabhakar Singh <PrabhakarKumarS@hexaware.com>
2025-10-24 17:35:20 +0530

    Button changed to green & red

---
+++ src/fluent/generated/sys_scope_privilege_1d8ecf553bf032d027891964c3e45a5b.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['1d8ecf553bf032d027891964c3e45a5b'],
+    operation: 'execute',
+    status: 'allowed',
+    targetName: 'GlideRecord.update',
+    targetScope: 'global',
+    targetType: 'scriptable',
+})
